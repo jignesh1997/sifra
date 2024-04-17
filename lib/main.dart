eb4915a1a37d4872d102f12ba24b1a02c3c0c63a
YudizAndroidJignesh <77387014+YudizAndroidJignesh@users.noreply.github.com>
2024-04-17 10:32:11 +0530

    done size fixing

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,23 @@ import 'package:clipboard/clipboard.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:get/get.dart';
 import 'package:sifra/screens/main/main_main_screen.dart';
+import 'package:window_manager/window_manager.dart';
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(400, 600),
+    center: true,
+    // Set other window options if needed
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(MyApp());
 }
 
@@ -14,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Clipboard App',
+      title: 'Sifra',
       home: MainScreen(),
     );
   }
--- lib/screens/main/main_main_screen.dart
+++ lib/screens/main/main_main_screen.dart
@@ -1,55 +1,82 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../android/android_screen.dart';
 import '../general_clipborad_to_file/clipboard_screen.dart';
+import 'package:window_manager/window_manager.dart';
 
 
 class MainScreen extends StatelessWidget {
+  var _isAlwaysOnTop = false.obs;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => AndroidClipboardMonitor());
-              },
-              child: Text('Android'),
+    return  Obx(
+      ()=> Scaffold(
+          appBar: AppBar(
+            title: Text('Main Screen'),
+          ),
+          body: Center(
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Row(
+                    children: [
+                      Text("Always on Top"),
+                      Checkbox(
+                        value: _isAlwaysOnTop.value,
+                        onChanged: (value) async {
+                            _isAlwaysOnTop.value = value!;
+                          await windowManager.setAlwaysOnTop(value!);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Get.to(() => AndroidClipboardMonitor());
+                      },
+                      child: Text('Android'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Get.to(() => ClipboardMonitor());
+                      },
+                      child: Text('Flutter'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Get.to(() => ClipboardMonitor());
+                      },
+                      child: Text('Genral'),
+                    ),
+                    // Add more options here
+                  ],
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => ClipboardMonitor());
-              },
-              child: Text('Flutter'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => ClipboardMonitor());
-              },
-              child: Text('Genral'),
-            ),
-            // Add more options here
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50,
-          child: Center(
-            child: Text(
-              'Developed by Jignesh Shakya with ❤️',
-              style: TextStyle(fontSize: 16),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Container(
+              height: 50,
+              child: Center(
+                child: Text(
+                  'Developed by Jignesh Shakya with ❤️',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
             ),
           ),
         ),
-      ),
+
     );
   }
 }
